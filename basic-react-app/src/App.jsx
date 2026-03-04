cf05c926588d863b7ea524f8f3bb1cfd4fe2a43e
sahil8303 <sahilkawadse2003@gmail.com>
2026-03-04 14:40:47 +0530

    Added React practice project

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css"
+import {Title} from "./Title.jsx";
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1>This is my First App</h1>
+      <Title/>;
+   </div>
+  );
+  
+}
+
+export default App
