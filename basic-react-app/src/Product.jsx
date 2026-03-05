9f518524fe594fd4a53040d70ac1a0007c2a781d
sahil8303 <sahilkawadse2003@gmail.com>
2026-03-05 12:04:50 +0530

    ch

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,11 +1,11 @@
 import "./Product.css"
 
-function Product(props){
-    console.log(props);
+function Product({title,price}){
+    
     return (
         <div className="Product">
-            <h3>{props.title}</h3>
-            <h5>Product Description</h5>
+            <h3>{title}</h3>
+            <h5>{price}</h5>
         </div>
     );
 
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,7 +3,7 @@ import Product from "./Product.jsx"
 function ProductTab(){
     return(
         <>
-        <Product title="phone"/>
+        <Product title="phone" price="1000"/>
         <Product title="Laptop"/>
         <Product title="Pen"/>
         </>
