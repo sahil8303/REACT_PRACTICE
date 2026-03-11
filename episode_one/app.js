edbe4ac7a534861c4b37c643a38ec1d94ab6a89b
sahil8303 <sahilkawadse2003@gmail.com>
2026-03-11 15:18:22 +0530

    updated

--- episode_one/app.js
+++ episode_one/app.js
@@ -1,11 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id},[
-    React.createElement("h1",null,"Hello World"),
-    React.createElement("p",null,"This is a simple React app.")
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
