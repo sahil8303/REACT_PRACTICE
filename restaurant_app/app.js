c0a4f64c85f9cbb4e436cae6577d51a38e5c0834
sahil8303 <sahilkawadse2003@gmail.com>
2026-03-16 15:55:37 +0530

    RES_CARD ADDED

--- restaurant_app/app.js
+++ restaurant_app/app.js
@@ -21,8 +21,10 @@ import logo from "./images/logo.png";
 const Header = ()=>{
     return (
         <div className="header">
-            <div className="logo">
-                <img src={logo} alt="logo" />
+            <div className="logo-container">
+                <img 
+                className='logo'
+                src="https://img.freepik.com/premium-vector/food-logo-design-concept-restaurant-logo-design_9850-5111.jpg" alt="logo" />
             </div>
             <div className="nav-items"> 
                 <ul>
@@ -35,10 +37,52 @@ const Header = ()=>{
         </div>
     )
 }
+
+const RestaurantCard = (props) => {
+    console.log(props);
+    
+    return (
+        <div className='res-card' style={{backgroundColor:'#f0f0f0'}}>
+            <img className='res-logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlg7JYWWJNnY-MJVGm02itthRtcc105HPt4Q&s" alt="Restaurant" />
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating} Stars</h4>
+            <h4>{props.deliveryTime}</h4>
+
+            
+        </div>
+    )
+}
+
+const Body =()=>{
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+            <RestaurantCard resName="Medhna Foods" cuisine="Biryani, North Indian" rating="4.5" deliveryTime="30 mins"/>
+            <RestaurantCard resName="Tasty Bites" cuisine="Italian, Pizza" rating="4.2" deliveryTime="25 mins"/>
+            <RestaurantCard resName='KFC' cuisine='Fast Food' rating='4.0' deliveryTime='20 mins'/>
+            <RestaurantCard resName="Spice Delight" cuisine="Indian, Mughlai" rating="4.3" deliveryTime="35 mins"/>
+            <RestaurantCard resName="Burger King" cuisine="Fast Food" rating="4.1" deliveryTime="25 mins"/>
+            <RestaurantCard resName="Sushi World" cuisine="Japanese, Sushi" rating="4.6" deliveryTime="40 mins"/>
+            <RestaurantCard resName="Pasta Palace" cuisine="Italian, Pasta" rating="4.4" deliveryTime="30 mins"/>
+            <RestaurantCard resName="Curry House" cuisine="Indian, Curry" rating="4.2" deliveryTime="30 mins"/>
+            <RestaurantCard resName="Taco Town" cuisine="Mexican, Tacos" rating="4.0" deliveryTime="20 mins"/>
+            <RestaurantCard resName="Noodle Nook" cuisine="Chinese, Noodles" rating="4.3" deliveryTime="25 mins"/>
+
+
+            </div>
+        </div>
+    )
+}
 const AppLayout = () => {
     return (
         <div className ="app">
+        
             <Header/>
+            <Body/>
+        
+        
         </div>
     )
 }
